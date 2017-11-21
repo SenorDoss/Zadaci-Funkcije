bcbe0dafa8db0457035bbe9ab52ec792f5f701fc
SenorDoss <luka.katal@gmail.com>
2017-11-21 12:03:53 +0100

    Create 3.c

---
+++ ZadFunkcije1/3.c
@@ -0,0 +1,17 @@
+//3. Učitavati brojeve dok su veći od 10. U funkciji ispitati parnost svakog od učitanih brojeva.
+#include <stdio.h>
+void parnost(int a) {
+  if (a%2==0) 
+    printf("%d je paran broj.",a);
+  else 
+    printf("%d nije paran broj.",a);
+}
+
+void main() {
+  printf("Upisi broj: "); scanf("%d",&a);
+  while (a < 10) {
+  parnost(a);
+  printf("Upisi broj: "); scanf("%d",&a);
+  }
+} 
+  
