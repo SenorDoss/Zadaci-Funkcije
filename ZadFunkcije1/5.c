eb5c4530814ec42397f0c3caebd83fa9a26cadac
SenorDoss <luka.katal@gmail.com>
2017-11-21 12:13:15 +0100

    Create 5.c

---
+++ ZadFunkcije1/5.c
@@ -0,0 +1,14 @@
+//5. Učitavati znakove dok se ne učita X. U funkciji ispisati ASCII kod svakog od učitanih znakova.
+#inclue<stdio.h>
+void ascii(char c) {
+  printf("ASCII kod od '%c' je: '%d'.",c,c); //Ako ova linija prijavljuje greske, makar ne bi trebala, ili stavite \ ispred ' ili maknite sve '
+}
+
+void main() {
+  char c;
+  printf("Upisi znak: "); scanf(" %c",&a);
+  while (c != 'x' && c != 'X') {
+  ascii(c);
+  printf("Upisi znak: "); scanf(" %c",&a);
+  }
+} 
